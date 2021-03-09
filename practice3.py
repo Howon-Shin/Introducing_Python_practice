3316a6fcc59f4661527f2297f6636dbcd970ee55
Howon <shinhw371@naver.com>
2021-03-09 17:36:09 +0900

    practice 3

---
+++ practice3.py
@@ -0,0 +1,12 @@
+# 3.1
+print(60*60)
+# 3.2
+seconds_per_hour = 60*60
+# 3.3
+print(24*seconds_per_hour)
+# 3.4
+seconds_per_day = 24*seconds_per_hour
+# 3.5
+print(seconds_per_day/seconds_per_hour)
+# 3.6
+print(seconds_per_day//seconds_per_hour)
